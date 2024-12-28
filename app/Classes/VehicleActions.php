ac4f824f314eab9017b647f6122b98a6d9f99e2a
rajatsaha18 <editorrajatdiu@gmail.com>
2024-12-28 17:26:29 +0600

    crud in vehicle list

---
+++ app/Classes/VehicleActions.php
@@ -0,0 +1,11 @@
+<?php
+
+interface VehicleActions
+{
+    public function addVehicle($data);
+    public function editVehicle($id,$data);
+    public function deleteVehicle($id);
+    public function getVehicles();
+}
+
+?>
